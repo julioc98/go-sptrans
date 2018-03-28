47487730a825d4af96bb2defbfc284c1fff080a6
julioc98 <julioc.santos98@gmail.com>
2018-03-28 14:55:10 -0300

    add: func Auth

--- sptrans.go
+++ sptrans.go
@@ -1,5 +1,11 @@
 package sptrans
 
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 // SPTrans é a pricipal estrutura onde se encontram as pricipar informaçoes para usar este pacote
 type SPTrans struct {
 	BasePath string
@@ -13,4 +19,24 @@ func New(token string) (sp *SPTrans) {
 		Token:    token,
 	}
 	return
+}
+
+// Auth autentica na API da SPTrans Olho Vivo
+func (sp *SPTrans) Auth() (bool, error) {
+
+	url := sp.BasePath + "/Login/Autenticar?token=" + sp.Token
+
+	resp, err := http.Post(url, "text/json", nil)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+	if strings.Trim(string(body), "/r/n") == "true" {
+		return true, nil
+	}
+	return false, nil
 }
