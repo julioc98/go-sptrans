e9a79b85b7a9d90ef11db02d7997fc3da70944b7
julioc98 <julioc.santos98@gmail.com>
2018-03-28 14:27:59 -0300

    add: func TestSPTrans_Auth

--- sptrans_test.go
+++ sptrans_test.go
@@ -1,6 +1,10 @@
 package sptrans
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 )
@@ -8,6 +12,20 @@ import (
 const (
 	token = "tokendetestesptransolhovivojc"
 )
+
+func mockingServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Path:", r.URL.Path)
+		switch r.URL.Path {
+
+		case "/Login/Autenticar":
+			rToken := r.URL.Query().Get("token")
+			if rToken == token {
+				fmt.Fprintf(w, "true")
+			}
+		}
+	}))
+}
 
 func TestNew(t *testing.T) {
 
@@ -35,6 +53,41 @@ func TestNew(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotSp := New(tt.args.token); !reflect.DeepEqual(gotSp, tt.wantSp) {
 				t.Errorf("New() = %v, want %v", gotSp, tt.wantSp)
+			}
+		})
+	}
+}
+
+func TestSPTrans_Auth(t *testing.T) {
+	type fields struct {
+		BasePath string
+		Token    string
+	}
+
+	mockBasePath := mockingServer().URL
+
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "Autenticando corretamente na API da SPTrans",
+			fields: fields{
+				BasePath: mockBasePath,
+				Token:    token,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sp := &SPTrans{
+				BasePath: tt.fields.BasePath,
+				Token:    tt.fields.Token,
+			}
+			if got := sp.Auth(); got != tt.want {
+				t.Errorf("SPTrans.Auth() = %v, want %v", got, tt.want)
 			}
 		})
 	}
